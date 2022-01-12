c2afcc662dc71cc9dcbcd7e5e8e7f7abf8391fe5
zieeco <zieecosam@gmail.com>
2022-01-12 15:12:15 +0100

    Add index.js file, and update the rest files

--- src/index.js
+++ src/index.js
@@ -1,41 +1,106 @@
+/* eslint-disable import/no-cycle */
+
 import './style.css';
+import statusUpdate from './statusUpdate.js';
 
 const taskList = document.querySelector('.task-list');
+const todoInput = document.getElementById('todo-input');
+const addToDoBtn = document.getElementById('add-todo-btn');
+let storedToDos = [];
+let editIndex = null;
 
-const storedToDos = [
-  {
-    index: 0,
-    description: 'Awesome Books with ES6',
-    completed: true,
-  },
-  {
-    index: 1,
-    description: 'ToDo List: add_list item',
-    completed: true,
-  },
-  {
-    index: 2,
-    description: 'ToDo List: add_Remove',
-    completed: true,
-  },
-  {
-    index: 3,
-    description: 'ToDo List: interactive-list',
-    completed: true,
-  },
-];
+const getToDos = () => {
+  if (localStorage.getItem('toDos') === null) {
+    storedToDos = [];
+  } else {
+    storedToDos = JSON.parse(localStorage.getItem('toDos'));
+  }
 
-const autoFillData = () => {
   let display = '';
-  storedToDos.forEach((task) => {
-    display += `<li class="p-sm-i flex b-b" id=${task.index}>
+  storedToDos.forEach((task, index) => {
+    const completed = task.completed ? 'line-through' : '';
+    display += `
+    <li class="p-sm-i flex b-b ${completed}" id=${index}>
     <div class="flex">
-    <input class=" mr-12" type="checkbox" id=${task.index} ${task.completed ? 'checked' : ''}>
-    <label type="text" for="${task.description}" class="label fs" id="${task.index}">${task.description}</label>
+    <input class=" checkbox mr-12" type="checkbox" id=${index} ${task.completed ? 'checked' : ''}>
+    <label for="${index}" class='label'>${task.description}</label>
     </div>
-    <i class="fas fa-ellipsis-v resize" id="${task.index}"></i>
+
+    <div class="">
+    <i class="fas fa-trash delete" id=${index}></i>
+    <i class="fas fa-edit edit " id=${index}></i>
+    </div>
     </li>`;
   });
   taskList.innerHTML = display;
 };
-autoFillData();
+
+const saveToDos = ({ description, completed = false }) => {
+  storedToDos = [];
+  if (localStorage.getItem('toDos') === null) {
+    storedToDos = [];
+  } else {
+    storedToDos = JSON.parse(localStorage.getItem('toDos'));
+  }
+
+  storedToDos.push({ description, completed });
+  localStorage.setItem('toDos', JSON.stringify(storedToDos));
+};
+
+const saveEdittedTask = (task) => {
+  const myTask = storedToDos[task];
+  myTask.description = todoInput.value;
+  localStorage.setItem('toDos', JSON.stringify(storedToDos));
+};
+
+const completedTask = (task) => {
+  const input = task.querySelector('input');
+  const label = task.querySelector('label');
+  const todoItem = storedToDos[task.id];
+  if (input.checked) {
+    todoItem.completed = true;
+    label.style.textDecoration = 'line-through';
+  } else {
+    todoItem.completed = false;
+    label.style.textDecoration = 'none';
+  }
+  localStorage.setItem('toDos', JSON.stringify(storedToDos));
+};
+
+const deleteTask = (task) => {
+  const item = storedToDos[task];
+  storedToDos = storedToDos.filter((todo) => todo !== item);
+  localStorage.setItem('toDos', JSON.stringify(storedToDos));
+  window.location.reload();
+};
+
+const editTask = (task) => {
+  editIndex = task;
+  const taskToEdit = storedToDos[task];
+  todoInput.value = taskToEdit.description;
+  todoInput.focus();
+};
+
+addToDoBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (!todoInput.value) {
+    return;
+  }
+
+  if (editIndex != null) {
+    saveEdittedTask(editIndex);
+    editIndex = null;
+  } else {
+    saveToDos({ description: todoInput.value, completed: false });
+  }
+
+  getToDos();
+  todoInput.value = '';
+});
+
+document.addEventListener('DOMContentLoaded', getToDos);
+taskList.addEventListener('click', statusUpdate);
+
+export {
+  deleteTask, completedTask, editTask,
+};
---
+++ src/statusUpdate.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-cycle */
+import { completedTask, deleteTask, editTask } from './index.js';
+
+const statusUpdate = (e) => {
+  const item1 = e.target;
+  // const item2 = e.target.parentElement;
+
+  if (item1.classList.contains('checkbox')) {
+    const itemm = item1.closest('li');
+    completedTask(itemm);
+  }
+
+  if (item1.classList.contains('delete')) {
+    const itemm = item1.parentElement.parentElement.id;
+    deleteTask(itemm);
+  }
+
+  if (item1.classList.contains('edit')) {
+    const itemm = item1.parentElement.parentElement.id;
+    editTask(itemm);
+  }
+};
+
+export default statusUpdate;
